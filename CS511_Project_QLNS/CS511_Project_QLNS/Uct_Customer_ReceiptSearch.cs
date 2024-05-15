80bec626e6a94bd85d73c848f033fd219cdf8519
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-05-15 11:00:09 +0700

    Added the handle if cannot find receipt code in Uct_Customer_ReceiptSearch

--- CS511_Project_QLNS/CS511_Project_QLNS/Uct_Customer_ReceiptSearch.cs
+++ CS511_Project_QLNS/CS511_Project_QLNS/Uct_Customer_ReceiptSearch.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,11 +14,18 @@ namespace CS511_Project_QLNS
 {
     public partial class Uct_Customer_ReceiptSearch : UserControl
     {
+        public SqlConnection sqlCon;
+        connection co = new connection();
+        public SqlCommand cmd;
+        string connect;
         public Uct_Customer_ReceiptSearch()
         {
             InitializeComponent();
             fpnl_receipt.WrapContents = true;
             fpnl_receipt.AutoScroll = true;
+
+            connect = co.connect;
+            sqlCon = new SqlConnection(connect);
         }
 
         private void btn_choosepic_Click(object sender, EventArgs e)
@@ -37,6 +45,20 @@ namespace CS511_Project_QLNS
                         ZXing.BarcodeReader rd = new ZXing.BarcodeReader();
                         Result result1 = rd.Decode(imgQRCode);
                         string decoded = result1.ToString().Trim();
+
+                        if (sqlCon.State != ConnectionState.Open) { sqlCon.Open(); }
+                        cmd = new SqlCommand();
+                        cmd.Connection = sqlCon;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = "SELECT COUNT(*) FROM TBL_CUS_RECEIPT WHERE ID_REC = " + decoded;
+                        int count = (int)(cmd.ExecuteScalar());
+                        if (count == 0)
+                        {
+                            MessageBox.Show("Sorry, there is no receipt with this code, please try another one", "Opps");
+                            return;
+                        }
+
+                        //handle the actual qr code
 
                         //imgQRCode.Dispose();
                     }
