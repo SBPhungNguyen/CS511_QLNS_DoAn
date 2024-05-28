a87262c684b0907684531ca8aaa3a9091ff89ac5
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-05-28 09:17:24 +0700

    code for changing button and loaddata(not done) for emp

--- CS511_Project_QLNS/CS511_Project_QLNS/Uct_Employee_Cashier.cs
+++ CS511_Project_QLNS/CS511_Project_QLNS/Uct_Employee_Cashier.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,72 @@ namespace CS511_Project_QLNS
 {
     public partial class Uct_Employee_Cashier : UserControl
     {
+        connection co = new connection();
+        public string connect;
+        public string pic_dir;
+        SqlConnection sqlCon;
+        SqlCommand cmd;
+
+        public Color color_btn_cate_normal = Color.Green;
+        public Color color_btn_cate_chosen = Color.FromArgb(0, 181, 0);
+
+        public int is_displayed_button;
         public Uct_Employee_Cashier()
         {
             InitializeComponent();
+            sqlCon = new SqlConnection(co.connect);
+            pic_dir = co.emp_dir;
+
+            is_displayed_button = 0;
+            btn_all.BackColor = color_btn_cate_chosen;
+            btn_cat1.BackColor = color_btn_cate_normal;
+            btn_cat2.BackColor = color_btn_cate_normal;
+        }
+        public void LoadData()
+        {
+            if (sqlCon.State == ConnectionState.Closed) { sqlCon.Open(); }
+            cmd = new SqlCommand();
+            cmd.Connection = sqlCon;
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "SELECT * FROM TBL_EMP";
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+
+            }
+            dr.Close();
+            sqlCon.Close();
+        }
+
+        private void btn_all_Click(object sender, EventArgs e)
+        {
+            if (is_displayed_button == 0)
+                return;
+            is_displayed_button = 0;
+            btn_all.BackColor = color_btn_cate_chosen;
+            btn_cat1.BackColor = color_btn_cate_normal;
+            btn_cat2.BackColor = color_btn_cate_normal;
+        }
+
+        private void btn_cat1_Click(object sender, EventArgs e)
+        {
+            if (is_displayed_button == 1)
+                return;
+            is_displayed_button = 1;
+            btn_all.BackColor = color_btn_cate_normal;
+            btn_cat1.BackColor = color_btn_cate_chosen;
+            btn_cat2.BackColor = color_btn_cate_normal;
+
+        }
+
+        private void btn_cat2_Click(object sender, EventArgs e)
+        {
+            if (is_displayed_button == 2)
+                return;
+            is_displayed_button = 2;
+            btn_all.BackColor = color_btn_cate_normal;
+            btn_cat1.BackColor = color_btn_cate_normal;
+            btn_cat2.BackColor = color_btn_cate_chosen;
         }
     }
 }
