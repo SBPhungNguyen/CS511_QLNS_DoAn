770f6bc20f4dfe6b011d2d9d4a5ef1ac26fe8131
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-05-28 09:20:43 +0700

    loaddata for emp (uct)

--- CS511_Project_QLNS/CS511_Project_QLNS/Uct_Emp_Cashier.cs
+++ CS511_Project_QLNS/CS511_Project_QLNS/Uct_Emp_Cashier.cs
@@ -12,9 +12,17 @@ namespace CS511_Project_QLNS
 {
     public partial class Uct_Emp_Cashier : UserControl
     {
+        public int id;
         public Uct_Emp_Cashier()
         {
             InitializeComponent();
+        }
+        public void LoadData(int id, Image pic, string name, string role)
+        {
+            this.id = id;
+            ptb_img.BackgroundImage = pic;
+            lbl_name.Text = name;
+            lbl_role.Text = role;
         }
     }
 }
