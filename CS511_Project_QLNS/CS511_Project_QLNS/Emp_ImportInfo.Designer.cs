49df1d3a7586a51c752f3f064026939b3e8a0497
SBPhungNguyen <22521148@gm.uit.edu.vn>
2024-06-10 17:45:46 +0700

    adjust Import Info UI

--- CS511_Project_QLNS/CS511_Project_QLNS/Emp_ImportInfo.Designer.cs
+++ CS511_Project_QLNS/CS511_Project_QLNS/Emp_ImportInfo.Designer.cs
@@ -60,20 +60,23 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.Color.Green;
             this.panel1.Controls.Add(this.btn_exit);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1300, 40);
+            this.panel1.Size = new System.Drawing.Size(1901, 50);
             this.panel1.TabIndex = 3;
             // 
             // btn_exit
             // 
+            this.btn_exit.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btn_exit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_exit.BackgroundImage")));
             this.btn_exit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_exit.Location = new System.Drawing.Point(1263, 5);
+            this.btn_exit.Location = new System.Drawing.Point(1853, 5);
             this.btn_exit.Name = "btn_exit";
-            this.btn_exit.Size = new System.Drawing.Size(30, 30);
+            this.btn_exit.Size = new System.Drawing.Size(40, 40);
             this.btn_exit.TabIndex = 18;
             this.btn_exit.TabStop = false;
             this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
@@ -82,6 +85,8 @@
             // 
             // customPanel1
             // 
+            this.customPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.customPanel1.BackColor = System.Drawing.SystemColors.Window;
             this.customPanel1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(138)))), ((int)(((byte)(93)))));
             this.customPanel1.Controls.Add(this.lbl_total);
@@ -93,138 +98,150 @@
             this.customPanel1.Controls.Add(this.lbl_code);
             this.customPanel1.Controls.Add(this.label4);
             this.customPanel1.Controls.Add(this.label1);
-            this.customPanel1.Location = new System.Drawing.Point(12, 124);
+            this.customPanel1.Location = new System.Drawing.Point(12, 209);
             this.customPanel1.Name = "customPanel1";
             this.customPanel1.Radius = 10;
-            this.customPanel1.Size = new System.Drawing.Size(1276, 139);
+            this.customPanel1.Size = new System.Drawing.Size(1876, 193);
             this.customPanel1.TabIndex = 4;
             this.customPanel1.Thickness = 5F;
             // 
             // lbl_total
             // 
             this.lbl_total.AutoSize = true;
-            this.lbl_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_total.Location = new System.Drawing.Point(947, 92);
+            this.lbl_total.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_total.Location = new System.Drawing.Point(1326, 142);
             this.lbl_total.Name = "lbl_total";
-            this.lbl_total.Size = new System.Drawing.Size(63, 22);
+            this.lbl_total.Size = new System.Drawing.Size(88, 32);
             this.lbl_total.TabIndex = 62;
             this.lbl_total.Text = "Code:";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(773, 92);
+            this.label7.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1129, 141);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(62, 22);
+            this.label7.Size = new System.Drawing.Size(91, 33);
             this.label7.TabIndex = 61;
             this.label7.Text = "Total:";
             // 
             // lbl_em_name
             // 
             this.lbl_em_name.AutoSize = true;
-            this.lbl_em_name.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_em_name.Location = new System.Drawing.Point(325, 92);
+            this.lbl_em_name.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_em_name.Location = new System.Drawing.Point(609, 142);
             this.lbl_em_name.Name = "lbl_em_name";
-            this.lbl_em_name.Size = new System.Drawing.Size(73, 22);
+            this.lbl_em_name.Size = new System.Drawing.Size(102, 32);
             this.lbl_em_name.TabIndex = 60;
             this.lbl_em_name.Text = "Codes:";
             // 
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label10.Location = new System.Drawing.Point(81, 92);
+            this.label10.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(317, 141);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(160, 22);
+            this.label10.Size = new System.Drawing.Size(246, 33);
             this.label10.TabIndex = 59;
             this.label10.Text = "Employee Name:";
             // 
             // lbl_date
             // 
             this.lbl_date.AutoSize = true;
-            this.lbl_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_date.Location = new System.Drawing.Point(947, 52);
+            this.lbl_date.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_date.Location = new System.Drawing.Point(1326, 91);
             this.lbl_date.Name = "lbl_date";
-            this.lbl_date.Size = new System.Drawing.Size(63, 22);
+            this.lbl_date.Size = new System.Drawing.Size(88, 32);
             this.lbl_date.TabIndex = 56;
             this.lbl_date.Text = "Code:";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(773, 52);
+            this.label6.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1129, 92);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(58, 22);
+            this.label6.Size = new System.Drawing.Size(86, 33);
             this.label6.TabIndex = 55;
             this.label6.Text = "Date:";
             // 
             // lbl_code
             // 
             this.lbl_code.AutoSize = true;
-            this.lbl_code.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_code.Location = new System.Drawing.Point(326, 52);
+            this.lbl_code.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_code.Location = new System.Drawing.Point(609, 89);
             this.lbl_code.Name = "lbl_code";
-            this.lbl_code.Size = new System.Drawing.Size(63, 22);
+            this.lbl_code.Size = new System.Drawing.Size(88, 32);
             this.lbl_code.TabIndex = 54;
             this.lbl_code.Text = "Code:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(81, 52);
+            this.label4.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(316, 88);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(96, 22);
+            this.label4.Size = new System.Drawing.Size(149, 33);
             this.label4.TabIndex = 53;
             this.label4.Text = "Import ID:";
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Green;
-            this.label1.Location = new System.Drawing.Point(581, 9);
+            this.label1.Location = new System.Drawing.Point(719, 24);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(138, 24);
+            this.label1.Size = new System.Drawing.Size(508, 36);
             this.label1.TabIndex = 52;
-            this.label1.Text = "General Info";
+            this.label1.Text = "General Information";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // lbl_title
             // 
-            this.lbl_title.AutoSize = true;
-            this.lbl_title.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbl_title.Font = new System.Drawing.Font("Tahoma", 25.8F, System.Drawing.FontStyle.Bold);
             this.lbl_title.ForeColor = System.Drawing.Color.Green;
-            this.lbl_title.Location = new System.Drawing.Point(541, 57);
+            this.lbl_title.Location = new System.Drawing.Point(573, 107);
             this.lbl_title.Name = "lbl_title";
-            this.lbl_title.Size = new System.Drawing.Size(248, 40);
+            this.lbl_title.Size = new System.Drawing.Size(827, 67);
             this.lbl_title.TabIndex = 52;
-            this.lbl_title.Text = "IMPORT INFO";
+            this.lbl_title.Text = "IMPORT INFORMATION";
+            this.lbl_title.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // customPanel3
             // 
+            this.customPanel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.customPanel3.BackColor = System.Drawing.SystemColors.Window;
             this.customPanel3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(138)))), ((int)(((byte)(93)))));
             this.customPanel3.Controls.Add(this.fpnl_detail);
             this.customPanel3.Controls.Add(this.customPanel4);
             this.customPanel3.Controls.Add(this.label3);
-            this.customPanel3.Location = new System.Drawing.Point(12, 279);
+            this.customPanel3.Location = new System.Drawing.Point(12, 412);
             this.customPanel3.Name = "customPanel3";
             this.customPanel3.Radius = 10;
-            this.customPanel3.Size = new System.Drawing.Size(1276, 509);
+            this.customPanel3.Size = new System.Drawing.Size(1876, 678);
             this.customPanel3.TabIndex = 53;
             this.customPanel3.Thickness = 5F;
             // 
             // fpnl_detail
             // 
-            this.fpnl_detail.Location = new System.Drawing.Point(37, 101);
+            this.fpnl_detail.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.fpnl_detail.BackColor = System.Drawing.Color.White;
+            this.fpnl_detail.Location = new System.Drawing.Point(44, 144);
             this.fpnl_detail.Name = "fpnl_detail";
-            this.fpnl_detail.Size = new System.Drawing.Size(1214, 395);
+            this.fpnl_detail.Size = new System.Drawing.Size(1788, 486);
             this.fpnl_detail.TabIndex = 55;
             // 
             // customPanel4
             // 
+            this.customPanel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.customPanel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(171)))), ((int)(((byte)(116)))));
             this.customPanel4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(101)))), ((int)(((byte)(69)))));
             this.customPanel4.Controls.Add(this.label16);
@@ -232,10 +249,10 @@
             this.customPanel4.Controls.Add(this.label12);
             this.customPanel4.Controls.Add(this.label13);
             this.customPanel4.Controls.Add(this.label14);
-            this.customPanel4.Location = new System.Drawing.Point(40, 46);
+            this.customPanel4.Location = new System.Drawing.Point(44, 78);
             this.customPanel4.Name = "customPanel4";
             this.customPanel4.Radius = 5;
-            this.customPanel4.Size = new System.Drawing.Size(1175, 45);
+            this.customPanel4.Size = new System.Drawing.Size(1754, 60);
             this.customPanel4.TabIndex = 54;
             this.customPanel4.Thickness = 2F;
             // 
@@ -291,21 +308,23 @@
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Green;
-            this.label3.Location = new System.Drawing.Point(577, 9);
+            this.label3.Location = new System.Drawing.Point(725, 22);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(156, 24);
+            this.label3.Size = new System.Drawing.Size(502, 43);
             this.label3.TabIndex = 53;
             this.label3.Text = "Import Details";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Emp_ImportInfo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1300, 800);
+            this.ClientSize = new System.Drawing.Size(1900, 1102);
             this.Controls.Add(this.customPanel3);
             this.Controls.Add(this.lbl_title);
             this.Controls.Add(this.customPanel1);
@@ -314,16 +333,15 @@
             this.Name = "Emp_ImportInfo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Emp_ImportInfo";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.btn_exit)).EndInit();
             this.customPanel1.ResumeLayout(false);
             this.customPanel1.PerformLayout();
             this.customPanel3.ResumeLayout(false);
-            this.customPanel3.PerformLayout();
             this.customPanel4.ResumeLayout(false);
             this.customPanel4.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
